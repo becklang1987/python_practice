5eb7940bfc6be774d8284f694a7dfef9d8212834
becklang1987 <119975424+becklang1987@users.noreply.github.com>
2024-03-31 16:29:15 +0800

    test

---
+++ test/program.py
@@ -0,0 +1,20 @@
+import sys
+
+from PIL import Image
+import os
+
+def picrefact(test):
+    os.chdir(test)
+    for pic in os.listdir():
+        f ,e = os.path.splitext(pic)
+        outfile = f + ".jpg"
+        print(f)
+        print(e)
+        if e == ".png":
+                with Image.open(pic) as im:
+                    im= im.rotate(-90).resize((200,200)).convert('RGB')
+                    im.save(outfile)
+
+
+    return
+picrefact(sys.argv[1])
---
+++ test/test1/readme2.py
@@ -0,0 +1,5 @@
+import os
+os.getcwd()
+os.chdir('../')
+os.getcwd()
+print(os.getcwd())
