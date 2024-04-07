515c6997296b5860b34dfa3409fc1da997f552b0
becklang1987 <119975424+becklang1987@users.noreply.github.com>
2024-04-08 04:30:34 +0800

    test

--- test/practice.py
+++ test/practice.py
@@ -8,7 +8,7 @@ def convertpic(tt):
     for pic in os.listdir(os.getcwd()):
         f,e = os.path.splitext(pic)
         print(re.search(r'^(ic).*', f))
-        if re.search(r'^(ic).*',f) == True:
+        if re.search(r'^(ic).*',f) != None:
             with Image.open(pic) as ff:
                 ff=ff.rotate(-90).resize((128,128))
                 ff.save("/opt/icons"+f+".jpg")
