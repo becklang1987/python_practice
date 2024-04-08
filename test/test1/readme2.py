75c91992ce9a483c79b74414aa6808c3b740ebdb
becklang1987 <119975424+becklang1987@users.noreply.github.com>
2024-04-09 01:54:50 +0800

    test

--- test/test1/readme2.py
+++ test/test1/readme2.py
@@ -1,5 +1,2 @@
-import os
-os.getcwd()
-os.chdir('../')
-os.getcwd()
-print(os.getcwd())
+item={"id":7,"car":{"car_make":"Lamborghini","car_model":"Murciélago","car_year":2003},"price":"$7267.94","total_sales":374}
+print(item['car']['car_year'])
